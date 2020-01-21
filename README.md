57a77ab80755c3723d4b1e7d97f0389d4299b307
Henry <phuongdm@coccoc.com>
2020-01-21 11:39:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# laravel_docker
+## Use docker compose for multi laravel projects
+
+- `cp env.example .env`
+- change config in .env file
+- create new laravel projects in `APP_CODE_PATH_HOST` default `../projects`
+- `docker-compose up -d` and enjoy :)
---
+++ mysql/docker-entrypoint-initdb.d/createdb.sql
@@ -0,0 +1,29 @@
+#
+# Copy createdb.sql.example to createdb.sql
+# then uncomment then set database name and username to create you need databases
+#
+# example: .env MYSQL_USER=appuser and need db name is myshop_db
+#
+#    CREATE DATABASE IF NOT EXISTS `myshop_db` ;
+#    GRANT ALL ON `myshop_db`.* TO 'appuser'@'%' ;
+#
+#
+# this sql script will auto run when the mysql container starts and the $DATA_PATH_HOST/mysql not found.
+#
+# if your $DATA_PATH_HOST/mysql exists and you do not want to delete it, you can run by manual execution:
+#
+#     docker-compose exec mysql bash
+#     mysql -u root -p < /docker-entrypoint-initdb.d/createdb.sql
+#
+
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+-- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+-- ALTER USER 'default'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
+
+CREATE DATABASE IF NOT EXISTS `vnecos` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `vnecos`.* TO 'henry'@'%' ;
+
+CREATE DATABASE IF NOT EXISTS `cevekgam` COLLATE 'utf8_general_ci' ;
+GRANT ALL ON `cevekgam`.* TO 'henry'@'%' ;
+
+FLUSH PRIVILEGES ;
